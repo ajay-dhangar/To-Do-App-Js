955ca8a8e35d8b21c04a6b01e4061b775d611786
Ajay-Dhangar <99037494+Ajay-Dhangar@users.noreply.github.com>
2023-04-14 17:01:51 +0530

    updates

--- demo.js
+++ demo.js
@@ -49,7 +49,11 @@ function renderContent() {
         content.done ? "checked" : ""
       }" id="content_${content.id}" onclick="doneTask(${content.id},${
         cards[i].id
-      })">${content.contentText}</li>`;
+      })">${
+        content.contentText
+      }      <svg xmlns="http://www.w3.org/2000/svg" class="editing" width="1em" height="1em" viewBox="0 0 16 16"><path fill="currentColor" d="M16 4s0-1-1-2s-1.9-1-1.9-1L12 2.1V0H0v16h12V8l4-4zm-9.7 7.4l-.6-.6l.3-1.1l1.5 1.5l-1.2.2zm.9-1.9l-.6-.6l5.2-5.2c.2.1.4.3.6.5zm6.9-7l-.9 1c-.2-.2-.4-.3-.6-.5l.9-.9c.1.1.3.2.6.4zM11 15H1V1h10v2.1L5.1 9L4 13.1L8.1 12L11 9v6z"></path></svg></li>
+
+        `;
     }
     ul.innerHTML = child;
   }
